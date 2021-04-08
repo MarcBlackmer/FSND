bed7691494782a38fa4c611cc20bb055f1dbad6e
Marc Blackmer <marc.blackmer@1nterrupt.com>
2021-04-08 11:14:23 -0400

    Added question delete

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -55,6 +55,14 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['total_questions'])
         self.assertTrue(data['categories'])
 
+    def test_deleteQuestion(self):
+        response = self.client().get(
+            '/questions/23/delete')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
