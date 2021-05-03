de12122d4227d8e8a4688652fb01f14c0dc45193
Marc Blackmer <marc.blackmer@1nterrupt.com>
2021-05-03 14:08:35 -0400

    Fix auth0 url

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'blackmer', // the auth0 domain prefix
+    url: 'blackmer.us', // the auth0 domain prefix
     audience: 'http://localhost:5000', // the audience set for the auth0 app
     clientId: 'jthK48uyr3GYsu02nRj7ugYuqS82eWVc', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100' // the base url of the running ionic application.
